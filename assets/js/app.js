a43e5db90ab9bb0eaff7f4befa786087db29337f
Binod Chaudhary <binod@rumsan.com>
2021-02-14 09:38:38 +0545

    basic setup and UI

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+console.log('Hello Folks');
+function isNumberKey(evt) {
+  var charCode = evt.which ? evt.which : evt.keyCode;
+  if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+    return false;
+  return true;
+}
